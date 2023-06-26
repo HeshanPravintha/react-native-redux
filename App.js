7cb5198ebc49408ce75b0bcf6c07b3904f89b7f5
HeshanPravintha <hpravintha@gmail.com>
2023-06-26 15:45:05 +0530

    Update.

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 // App.js
 import React from 'react';
-import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './src/redux/reducers';
 import ProductDetails from './src/screens/ProductDetails';
